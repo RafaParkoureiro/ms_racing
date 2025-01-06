40fd35e92af36b3ccad6435dd0437d11e812158d
Daniel Dias <81629592+v2denny@users.noreply.github.com>
2025-01-06 22:05:07 +0000

    Add files via upload

---
+++ custom_metrics.py
@@ -0,0 +1,52 @@
+import numpy as np
+from stable_baselines3.common.callbacks import BaseCallback
+
+
+class CustomMetricsCallback(BaseCallback):
+    def __init__(self, verbose=0):
+        super(CustomMetricsCallback, self).__init__(verbose)
+        self.metrics = {
+            "episode_length": [],
+            "episode_reward": [],
+            "proximity_time": [],
+            "on_track_time": [],
+            "off_track_time": [],
+        }
+
+    def _on_step(self) -> bool:
+        # Collect environment info
+        infos = self.locals.get("infos", [])
+        for info in infos:
+            if "episode_length" in info:
+                self.metrics["episode_length"].append(info["episode_length"])
+                self.metrics["episode_reward"].append(info["episode_reward"])
+                self.metrics["proximity_time"].append(info["proximity_time"])
+                self.metrics["on_track_time"].append(info["on_track_time"])
+                self.metrics["off_track_time"].append(info["off_track_time"])
+
+        # Log every 1000 steps (adjustable)
+        if self.n_calls % 1000 == 0:
+            if self.metrics["episode_length"]:
+                self.logger.record(
+                    "custom/mean_episode_length",
+                    np.mean(self.metrics["episode_length"]),
+                )
+                self.logger.record(
+                    "custom/mean_episode_reward",
+                    np.mean(self.metrics["episode_reward"]),
+                )
+                self.logger.record(
+                    "custom/mean_proximity_time",
+                    np.mean(self.metrics["proximity_time"]),
+                )
+                self.logger.record(
+                    "custom/mean_on_track_time",
+                    np.mean(self.metrics["on_track_time"]),
+                )
+                self.logger.record(
+                    "custom/mean_off_track_time",
+                    np.mean(self.metrics["off_track_time"]),
+                )
+            # Clear the metrics
+            self.metrics = {key: [] for key in self.metrics}
+        return True
